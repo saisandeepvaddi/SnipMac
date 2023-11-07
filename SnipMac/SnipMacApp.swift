5639be9de35d6e4e1bcf144be04749aff2a0a3f1
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-06 21:31:06 -0800

    Initial Commit

---
+++ SnipMac/SnipMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipMacApp.swift
+//  SnipMac
+//
+//  Created by Sai Sandeep Vaddi on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SnipMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
