04171e83bae5508f3f9a4ec2377acb9ac91f48ec
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-11 19:31:58 -0800

    Some working drag and drop

---
+++ SnipMac/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  SnipMac
+//
+//  Created by Sai Sandeep Vaddi on 11/10/23.
+//
+
+import Foundation
---
+++ SnipMac/OverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  OverlayView.swift
+//  SnipMac
+//
+//  Created by Sai Sandeep Vaddi on 11/10/23.
+//
+
+import SwiftUI
+
+struct OverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverlayView()
+}
