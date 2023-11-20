e320b25fe2e858e5f7087b5734ca425f694e6012
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-19 19:33:29 -0800

    Add to menubar

--- SnipMac/AppDelegate.swift
+++ SnipMac/AppDelegate.swift
@@ -2,60 +2,7 @@
 //  AppDelegate.swift
 //  SnipMac
 //
-//  Created by Sai Sandeep Vaddi on 11/10/23.
+//  Created by Sai Sandeep Vaddi on 11/19/23.
 //
 
-import AppKit
-import Cocoa
-import Combine
-import SwiftUI
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    static var shared: AppDelegate?
-
-    var overlayWindow: NSWindow?
-    var mainWindow: NSWindow?
-
-    override init() {
-        super.init()
-        AppDelegate.shared = self
-    }
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        print("AppDelegate is initialized.")
-    }
-
-    func showOverlayWindow(captureType: CaptureType, screenRecorder: ScreenRecorder? = nil) {
-        if overlayWindow == nil {
-            createOverlayWindow(captureType: captureType, screenRecorder: screenRecorder)
-        }
-        overlayWindow?.makeKeyAndOrderFront(nil)
-    }
-
-    func hideOverlayWindow() {
-        overlayWindow?.orderOut(nil)
-    }
-
-    func hideMainWindow() {
-        mainWindow?.orderOut(nil)
-    }
-
-    func showMainWindow() {
-        mainWindow?.makeKeyAndOrderFront(nil)
-    }
-
-    private func createOverlayWindow(captureType: CaptureType, screenRecorder: ScreenRecorder? = nil) {
-        let screenRect = NSScreen.main?.frame ?? NSRect(x: 0, y: 0, width: 0, height: 0)
-        overlayWindow = NSWindow(
-            contentRect: screenRect,
-            styleMask: [.borderless],
-            backing: .buffered, defer: false)
-        overlayWindow?.isOpaque = false
-        overlayWindow?.backgroundColor = NSColor.clear
-        overlayWindow?.level = .screenSaver
-        overlayWindow?.ignoresMouseEvents = false
-
-        let contentView = ScreenshotOverlayView(captureType: captureType, screenRecorder: screenRecorder ?? ScreenRecorder(), appState: appState ?? AppState())
-        overlayWindow?.contentView = NSHostingView(rootView: contentView)
-    }
-}
+import Foundation
---
+++ SnipMac/MenubarView.swift
@@ -0,0 +1,18 @@
+//
+//  MenubarView.swift
+//  SnipMac
+//
+//  Created by Sai Sandeep Vaddi on 11/19/23.
+//
+
+import SwiftUI
+
+struct MenubarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenubarView()
+}
