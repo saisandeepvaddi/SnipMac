d8fb9d3dc63aa057c3446b9a325099522f47d453
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-19 16:58:20 -0800

    clean up

--- SnipMac/Utils.swift
+++ SnipMac/Utils.swift
@@ -19,33 +19,15 @@ func withMainWindowClosed(_ action: @escaping () -> Void) {
     }
 }
 
-func scaleRect(input: CGRect) -> CGRect {
-    let screen = NSScreen.main
-    guard let screen = screen else {
-        return input
-    }
-    let scaleFactor = 2.0
-    let scaledRect = CGRect(
-        x: input.minX * scaleFactor,
-        y: input.minY * scaleFactor,
-        width: input.width * scaleFactor,
-        height: input.height * scaleFactor
-    )
-    return scaledRect
-}
-
 func calculateCropRect(from selectedRect: CGRect) -> CGRect {
     guard let screen = NSScreen.main else { return CGRect.zero }
     let scaleFactor = screen.backingScaleFactor
 
-    // Assuming selectedRect is in global screen coordinates
-    // Convert to bottom-left origin used by macOS screen coordinates
     let flippedRect = CGRect(x: selectedRect.origin.x,
                              y: screen.frame.height - selectedRect.origin.y - selectedRect.height,
                              width: selectedRect.width,
                              height: selectedRect.height)
 
-    // Apply scale factor for Retina displays
     let scaledRect = CGRect(x: flippedRect.origin.x * scaleFactor,
                             y: flippedRect.origin.y * scaleFactor,
                             width: flippedRect.width * scaleFactor,
